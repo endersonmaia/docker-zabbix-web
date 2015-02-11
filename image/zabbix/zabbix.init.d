5dd08b40a45198fc1809af8c00ad6396b4370911
Enderson Maia <endersonmaia@gmail.com>
2015-02-11 14:19:50 -0300

    Make it work with MySQL and PostgreSQL.

--- image/zabbix/zabbix.init.d
+++ image/zabbix/zabbix.init.d
@@ -1,23 +1,17 @@
 #!/bin/bash
 
+###
+# Database
+###
+
 DB_HOST=${DB_HOST:-}
-DB_PORT=${DB_PORT:-3306}
+DB_PORT=${DB_PORT:-}
 DB_NAME=${DB_NAME:-}
 DB_USER=${DB_USER:-}
 DB_PASS=${DB_PASS:-}
-DB_TYPE=${DB_TYPE:-MYSQL}
+DB_TYPE=${DB_TYPE:-}
 
-ZBX_SERVER=${ZBX_SERVER:-}
-ZBX_SERVER_PORT=${ZBX_SERVER_PORT:-10051}
-ZBX_SERVER_NAME=${ZBX_SERVER_NAME:-zabbix_server}
-
-if [ -n ${ZBX_PORT_10051_TCP_ADDR} ]; then
-  ZBX_SERVER=${ZBX_PORT_10051_TCP_ADDR}
-  ZBX_SERVER_PORT=${ZBX_PORT_10051_TCP_PORT}
-fi
-
-SERVER_NAME=${SERVER_NAME:-localhost}
-
+# MySQL
 if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ]; then
   DB_TYPE=MYSQL
   DB_HOST=${DB_HOST:-${MYSQL_PORT_3306_TCP_ADDR}}
@@ -28,6 +22,22 @@ if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ]; then
   DB_NAME=${DB_NAME:-${MYSQL_ENV_MYSQL_DATABASE}}
 fi
 
+# Postgres
+if [ -n "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
+  DB_TYPE=POSTGRESQL
+  DB_HOST=${DB_HOST:-${POSTGRES_PORT_5432_TCP_ADDR}}
+  DB_PORT=${DB_PORT:-${POSTGRES_PORT_5432_TCP_PORT}}
+
+  DB_USER=${DB_USER:-${POSTGRES_ENV_POSTGRES_USER}}
+  DB_PASS=${DB_PASS:-${POSTGRES_ENV_POSTGRES_PASSWORD}}
+  DB_NAME=${DB_NAME:-${POSTGRES_ENV_POSTGRES_USER}}  
+fi
+
+if [ -z "${DB_TYPE}" ]; then
+  echo "You have to link to a mysql or postgres container, or inform the database type and configuration via environment variables."
+  exit 1
+fi
+
 sed 's/{{DB_HOST}}/'"${DB_HOST}"'/' -i /etc/zabbix/web/zabbix.conf.php
 sed 's/{{DB_PORT}}/'"${DB_PORT}"'/' -i /etc/zabbix/web/zabbix.conf.php
 sed 's/{{DB_NAME}}/'"${DB_NAME}"'/' -i /etc/zabbix/web/zabbix.conf.php
@@ -35,7 +45,25 @@ sed 's/{{DB_USER}}/'"${DB_USER}"'/' -i /etc/zabbix/web/zabbix.conf.php
 sed 's/{{DB_PASS}}/'"${DB_PASS}"'/' -i /etc/zabbix/web/zabbix.conf.php
 sed 's/{{DB_TYPE}}/'"${DB_TYPE}"'/' -i /etc/zabbix/web/zabbix.conf.php
 
+###
+# Web
+###
+
+SERVER_NAME=${SERVER_NAME:-localhost}
 sed 's/{{SERVER_NAME}}/'"${SERVER_NAME}"'/' -i /etc/nginx/sites-enabled/zabbix.conf
+
+ZBX_SERVER=${ZBX_SERVER:-}
+ZBX_SERVER_PORT=${ZBX_SERVER_PORT:-10051}
+ZBX_SERVER_NAME=${ZBX_SERVER_NAME:-zabbix_server}
+
+###
+# Zabbix Server
+###
+
+if [ -n ${ZBX_PORT_10051_TCP_ADDR} ]; then
+  ZBX_SERVER=${ZBX_PORT_10051_TCP_ADDR}
+  ZBX_SERVER_PORT=${ZBX_PORT_10051_TCP_PORT}
+fi
 
 sed 's/{{ZBX_SERVER}}/'"${ZBX_SERVER}"'/' -i /etc/zabbix/web/zabbix.conf.php
 sed 's/{{ZBX_SERVER_PORT}}/'"${ZBX_SERVER_PORT}"'/' -i /etc/zabbix/web/zabbix.conf.php
--- image/zabbix/zabbix.sh
+++ image/zabbix/zabbix.sh
@@ -6,7 +6,7 @@ set -x
 curl -O http://repo.zabbix.com/zabbix/2.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.4-1+trusty_all.deb
 dpkg -i zabbix-release_2.4-1+trusty_all.deb
 apt-get update
-$minimal_apt_get_install zabbix-frontend-php mysql-client
+$minimal_apt_get_install zabbix-frontend-php php5-mysql php5-pgsql
 
 cp /build/zabbix/zabbix.conf.php /etc/zabbix/web/zabbix.conf.php
-cp /build/zabbix/zabbix.init.d /etc/my_init.d/00-zabbix.sh
+cp /build/zabbix/zabbix.init.d /etc/my_init.d/10-zabbix.sh
